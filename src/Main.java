bdbfe63f2ca7825d8ece9d3c5cc514c8958f34ed
TengJo <tengjh@mail.uc.edu>
2022-02-05 20:53:07 -0500

    Start

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main
 
     public static void main(String[] args)
     {
-	
+	double itemPrice;
+    double salesTax;
+        itemPrice = 59.99;
+        salesTax = itemPrice *.05;
+
+        System.out.println("The price of the item is " + itemPrice + " and the tax is " + salesTax);
     }
 }
